44f86841c21c811944ce371703c958f2fe279c9a
praveen <prrraveen@gmail.com>
2016-08-13 01:03:51 +0530

    added Accelstepper lib Run motor with fixed speed fixed no steps per click

--- arduino/src/axisDirectionStruct.h
+++ arduino/src/axisDirectionStruct.h
@@ -1,5 +1,4 @@
 struct AxisAndDirection{
 	char axis;
 	int direction;
-
 };
--- arduino/src/sketch.ino
+++ arduino/src/sketch.ino
@@ -1,13 +1,37 @@
+#include "AccelStepper.h"
 #include "axisDirectionStruct.h" //importing AxisAndDirection 
-#define led 13
+
 int incomingByte = 0;   // for incoming serial data
-bool flag = false;
+int stepsPerKeyStrock = 50;
+int speed = 15;
+// The X Stepper pins
+#define STEPPER_DIR_PIN 3
+#define STEPPER_STEP_PIN 2
+
+// Define some stepper  and the pins the will use
+AccelStepper stepper(AccelStepper::DRIVER, STEPPER_STEP_PIN, STEPPER_DIR_PIN);
 
 void setup()
 {
-	pinMode(led,OUTPUT);
 	Serial.begin(9600);
+	stepper.setMaxSpeed(40);
 }
+
+void blockingRunSpeedToPosition(long position)
+{
+	/*
+		runSpeedToPosition is non blocking.
+		You must call this as frequently as possible, but at least once per step interval,		
+		But we want blocking so we have to implement own loop using while
+	*/
+
+	stepper.setCurrentPosition(0);
+    stepper.moveTo(position);
+    stepper.setSpeed(speed);
+    while (stepper.distanceToGo() != 0)
+      stepper.runSpeedToPosition();
+}
+
 
 void loop()
 {
@@ -15,15 +39,14 @@ void loop()
 		//incoming data is stored in buffer. Serial.reads reads 1 byte from it. if not read, code under Serial.available will be executed with every spin cycle. 
 		incomingByte = Serial.parseInt();
 		AxisAndDirection axisAndDirection = getAxisAndDirection(incomingByte);
-		if(axisAndDirection.axis == 'x') {
 
-			bool ledStatus = false;
-			ledStatus = toggle_led();
-			//Serial.print(ledStatus);
-			digitalWrite(led, ledStatus);
+		if(axisAndDirection.axis == 'x') {
+			if(axisAndDirection.direction == -1) {
+				stepsPerKeyStrock = -stepsPerKeyStrock;
+			}
+
+			blockingRunSpeedToPosition(stepsPerKeyStrock);
 		}
-		Serial.print(axisAndDirection.axis);
-		Serial.print(axisAndDirection.direction);
 	}
 }
 
@@ -45,9 +68,4 @@ AxisAndDirection getAxisAndDirection(int incomingByte){
 			return {'A',0};
 			break; 
 	}
-}
-
-bool toggle_led() {
-	flag = !flag;
-	return flag;
 }
